9ce63aba4192e7e8c874f1b1aeec91bdd767a060
Aleksa Markovic <malexa96@gmail.com>
2022-09-24 19:48:12 +0200

    Implement request end detection in Lua

--- lua/lua_entry.lua
+++ lua/lua_entry.lua
@@ -1,11 +1,26 @@
+local client = {}
+
+client.CONSTS = {
+	requEnd = "\r\n\r\n",
+}
+
+client.buffer = ""
+
+function client:readChunk(chunkStr)
+	self.buffer = self.buffer .. chunkStr
+
+	if chunkStr == self.CONSTS.requEnd then
+		self:handleClientRequest()
+	end
+end
+
+function client:handleClientRequest()
+	print(self.buffer)
+	self.buffer = ""
+end
+
 function ReceivedFromNetwork(buf, fd)
-        print("From Lua: ", buf, fd)
-
-        local str = [[{"data" : [ 0: Object { "name": "Zasya"},
-                1: { "name": "Syake Sushi Hurghada"},
-                "2": { "name": "Sofra", latitude: "26.988743", … }
-                "3": { "name": "Cratus Premium Restaurant"}]} ]]
-
-        print("Sending " .. str)
-        SendToNetwork(str, fd)
+	client:readChunk(buf)
+        --print("Sending " .. str)
+        --SendToNetwork(str, fd)
 end
